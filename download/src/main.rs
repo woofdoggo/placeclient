ad8b04159ea0272ced17a62e5a36add61552f128
woofdoggo <woofwoofdoggo@protonmail.com>
2022-04-02 19:01:41 -0400

    kinda works
    download is too slow

--- download/src/main.rs
+++ download/src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::TcpListener, io::Read, fs::File};
+use std::{net::TcpListener, io::{Read, BufReader, BufRead}, fs::File};
 
 use chrono::Timelike;
 use flate2::{write::GzEncoder, Compression};
@@ -6,14 +6,14 @@ use tar::{Header, Builder};
 
 fn create_builder() -> Builder<GzEncoder<File>> {
     let millis = chrono::Utc::now().timestamp_millis();
-    let n = format!("/mnt/hdd/place/{}.tar.gz", millis);
+    let n = format!("/mnt/hdd/place2/{}.tar.gz", millis);
     let gz = File::create(n);
     let enc = GzEncoder::new(gz.unwrap(), Compression::default());
 
     return Builder::new(enc);
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
     let (ctx, crx) = std::sync::mpsc::channel::<String>();
     let (ttx, mut trx) = tokio::sync::mpsc::channel::<(Vec<u8>, String)>(32);
@@ -24,22 +24,21 @@ async fn main() {
         println!("accepted connection: {}", addr);
 
         stream.set_read_timeout(None).unwrap();
+        let mut buf = String::new();
+        let mut br = BufReader::new(stream);
         loop {
-            let mut buf: Vec<u8> = Vec::with_capacity(128);
-            buf.resize(128, 0);
+            let bytes = br.read_line(&mut buf).unwrap();
 
-            let bytes = stream.read(&mut buf).unwrap();
-            if bytes == 0 {
-                println!("proxy disconnected");
-                return;
+            if bytes > 0 {
+                println!("added");
+                ctx.send(String::from_utf8(buf.as_bytes().to_vec()).unwrap()).unwrap();
             }
-
-            ctx.send(String::from_utf8(buf[..bytes].to_vec()).unwrap()).unwrap();
+            buf.clear();
         }
     });
 
     std::thread::spawn(move || {
-        let mut last_minute = chrono::Utc::now().minute();
+        let mut in_tar = 0;
         let mut tb = create_builder();
 
         loop {
@@ -55,11 +54,12 @@ async fn main() {
 
             tb.append(&header, &res.to_vec()[..]).unwrap();
 
-            let current_minute = chrono::Utc::now().minute();
-            if current_minute != last_minute {
+            in_tar += 1;
+
+            if in_tar > 128 {
                 tb.finish().unwrap();
                 tb = create_builder();
-                last_minute = current_minute;
+                in_tar = 0;
 
                 println!("finished archive!");
             }
@@ -80,7 +80,9 @@ async fn main() {
                 .unwrap();
 
             let buftrim = &buf.split("/").last().unwrap();
+            println!("waiting...");
             ttxc.send((res.to_vec(), buftrim.to_string())).await.unwrap();
+            println!("channel sent");
         }).await.unwrap();
     }
 }
--- ws-scrape/src/main.rs
+++ ws-scrape/src/main.rs
@@ -1,14 +1,23 @@
-use std::{net::TcpStream, io::Write, fs::File};
+use std::{net::TcpStream, io::Write, fs::{File, OpenOptions}};
 
-use tungstenite::Message;
+use websocket::header::Headers;
 
 const ENDPOINT: &str = "wss://gql-realtime-2.reddit.com/query";
 
 #[tokio::main]
 async fn main() {
+    loop {
+        match go().await { _ => () };
+
+        // sleep 10sec if connection fails
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
+}
+
+async fn go() -> Result<(), Box<dyn std::error::Error>> {
     /*
     let client = reqwest::Client::new();
-    let resp = client.get(format!(
+    let resp = client.post(format!(
             "https://www.reddit.com/api/v1/access_token?grant_type=password&username={}&password={}",
             include_str!("../username.auth"), include_str!("../password.auth")
         ))
@@ -19,23 +28,52 @@ async fn main() {
     println!("{:?}", resp.text().await.unwrap());
     return;*/
 
-    let (mut ws, _) = tungstenite::connect(ENDPOINT).unwrap();
+    let mut headers = Headers::new();
+    headers.set_raw("Origin", vec![b"https://www.reddit.com".to_vec()]);
+    headers.set_raw("Host", vec![b"gql-realtime-2.reddit.com".to_vec()]);
 
-    // TODO: auth and send connection init message with bearer token
-    ws.write_message(Message::Text(format!(
-        "{{\"type\": \"connection_init\", \"payload\": {{\"Authorization\": \"{}\"}}}}",
-        include_str!("../token.auth")
-    ))).unwrap();
-    ws.write_message(Message::Text(include_str!("start.json").to_string())).unwrap();
+    let mut ws = websocket::ClientBuilder::new(ENDPOINT)
+        .unwrap()
+        .custom_headers(&headers)
+        .connect_secure(None)
+        .unwrap();
 
-    //let mut tc: TcpStream = TcpStream::connect(("127.0.0.1", 8001)).unwrap();
-    let mut log: File = File::create("/mnt/hdd/place.log").unwrap();
+    let t = format!(
+        "{{ \"type\": \"connection_init\", \"payload\": {{ \"Authorization\": \"{}\"}} }}",
+        include_str!("../token.auth").strip_suffix("\n").unwrap());
+
+    ws.send_message(&websocket::Message::text(t)).unwrap();
+
+    ws.send_message(&websocket::Message::text(
+            include_str!("start.json").to_string()
+    )).unwrap();
+
+    let mut tc: TcpStream = TcpStream::connect(("127.0.0.1", 8001)).unwrap();
+    let mut log: File = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("/mnt/hdd/place.log").unwrap();
+    log.write_all(format!("start session {}\n", chrono::Utc::now().timestamp_millis()).as_bytes()).unwrap();
 
     // receive messages over websocket
     loop {
-        let msg = ws.read_message().unwrap();
-        if let Message::Text(txt) = msg {
-            let j = serde_json::to_value(txt).unwrap();
+        let msg = ws.recv_message()?;
+        if let websocket::OwnedMessage::Text(txt) = msg {
+            let j: serde_json::Value = serde_json::from_str(&txt).unwrap();
+            match j.get("type") {
+                Some(v) => match v.as_str() {
+                    Some(v) => match v {
+                        "connection_ack" => continue,
+                        "connection_error" => return Ok(()),
+                        "ka" => continue,
+                        "data" => (),
+                        a => println!("wtf {}", a)
+                    },
+                    _ => continue
+                },
+                _ => continue
+            };
+
             let data = j.as_object()
                         .unwrap()
                         .get("payload")
@@ -58,33 +96,60 @@ async fn main() {
             let typename = data.get("__typename").unwrap().as_str().unwrap();
             match typename {
                 "ConfigurationMessageData" => {
-                    // TODO
+                    let canvases = data.get("canvasConfigurations")
+                        .unwrap()
+                        .as_array()
+                        .unwrap();
+
+                    for i in canvases {
+                        let tag = i.get("index").unwrap().as_i64().unwrap();
+                        let mut msg = include_str!("canvas.json").to_string();
+                        msg = msg.replace("PUTTHETAGHERE", &tag.to_string());
+                        ws.send_message(&websocket::Message::text(msg)).unwrap();
+
+                        let msg = format!(
+                            "canvas {} {} {}\n",
+                            tag,
+                            i.get("dx").unwrap().as_i64().unwrap(),
+                            i.get("dy").unwrap().as_i64().unwrap(),
+                        );
+
+                        log.write_all(msg.as_bytes()).unwrap();
+                    }
                 },
                 "DiffFrameMessageData" => {
-                    let url = data.get("name").unwrap().as_str().unwrap();
-                    let time = data.get("currentTimestamp").unwrap().as_u64().unwrap();
-                    //tc.write_all(url.as_bytes()).unwrap();
+                    let mut url = data.get("name").unwrap().as_str().unwrap().to_string();
+                    url.push('\n');
+                    
+                    let time = data.get("currentTimestamp").unwrap().as_f64().unwrap();
 
-                    let msg = format!("diff {} {}", time, url);
+                    if url.ends_with("image") { continue; }
+                    tc.write_all(url.as_bytes()).unwrap();
+
+                    let msg = format!("diff {} {}", time, url.split("/").last().unwrap());
                     log.write_all(msg.as_bytes()).unwrap();
-                    println!("{}", msg);
+                    println!("{}", msg.strip_suffix("\n").unwrap());
                 },
                 "FullFrameMessageData" => {
-                    let url = data.get("name").unwrap().as_str().unwrap();
-                    let time = data.get("currentTimestamp").unwrap().as_u64().unwrap();
-                    //tc.write_all(url.as_bytes()).unwrap();
+                    let mut url = data.get("name").unwrap().as_str().unwrap().to_string();
+                    url.push('\n');
 
-                    let msg = format!("full {} {}", time, url);
+                    let time = data.get("timestamp").unwrap().as_f64().unwrap();
+
+                    if url.ends_with("image") { continue; }
+                    tc.write_all(url.as_bytes()).unwrap();
+
+                    let msg = format!("full {} {}", time, url.split("/").last().unwrap());
                     log.write_all(msg.as_bytes()).unwrap();
-                    println!("{}", msg);
+                    println!("{}", msg.strip_suffix("\n").unwrap());
                 },
                 a => {
                     println!("bad message type: {}", a);
-                    log.write_all(format!("bad_message {}", a).as_bytes()).unwrap();
+                    log.write_all(format!("bad_message {}\n", a).as_bytes()).unwrap();
                 }
             }
         } else {
-            log.write_all(b"bad_message_json").unwrap();
+            log.write_all(b"bad_message_json\n").unwrap();
         }
     }
 }
